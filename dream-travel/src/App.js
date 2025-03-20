2ab9bbcd375b3f1713b3179234a236c6267026fd
Evelyn Monteros <Monterosvaca@gmail.com>
2025-03-20 09:12:06 -0400

    website app repo

--- dream-travel/src/App.js
+++ dream-travel/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
    <div>
       <h1>Hey There</h1>
-      <h2> Make sure that if you have more then one then its in something</h2>
+      <h2> Hey class</h2>
    </div>
   );
 }
